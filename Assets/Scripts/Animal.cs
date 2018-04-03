79690630de884bdddebc41ba54755208bb1843e0
PurpleNinjaLink <jawhittom@gmail.com>
2018-04-03 14:35:29 -0500

    First Scripts
    Zoo Event and Animal scripts written

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour 
+{
+    /// <summary>
+    /// The type of animal (lion, penguin, etc.)
+    /// </summary>
+    public string Species { get; private set; }
+    /// <summary>
+    /// The animal's happiness.
+    /// Should be a value between 0 and 1 (percentage-based)
+    /// </summary>
+    public float Happiness { get; private set; }
+    /// <summary>
+    /// Cost of food per animal per day
+    /// </summary>
+    public float FoodCost { get; private set; }
+    /// <summary>
+    /// Minimum comfortable temperature for the animal
+    /// </summary>
+    public float MinTemp { get; private set; }
+	/// <summary>
+    /// Maximum comfortable temperature for the animal
+    /// </summary>
+    public float MaxTemp { get; private set; }
+    /// <summary>
+    /// Total number of animals on display
+    /// </summary>
+    public int NumberInExhibit { get; private set; }
+    /// <summary>
+    /// Is an animal injured?
+    /// </summary>
+    public bool IsInjured { get; private set; }
+
+    //TODO Add a reference to the current enclosure in use for its stats/modifiers
+
+    /// <summary>
+    /// Make calculations for the day
+    /// </summary>
+    public void DailyRoutine()
+    {
+        UpdateHappiness();
+        CheckInjury();
+    }
+
+    private void CheckInjury()
+    {
+        float random = Random.Range(0.01f, 1f);
+        if (random < Happiness)
+            IsInjured = true;
+    }
+
+    private void UpdateHappiness()
+    {
+        // TODO Implement enclosure happiness modifier
+        if (!IsInjured)
+            Happiness *= (.99f - (.01f * NumberInExhibit));
+        else
+            Happiness *= (.75f - (.01f * NumberInExhibit));
+    }
+}
---
+++ Assets/Scripts/ZooEvent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IZooEvent
+{
+    /// <summary>
+    /// The description of the event.
+    /// Used in the event panel.
+    /// </summary>
+    string DescriptionText { get; set; }
+    /// <summary>
+    /// The text for option one.
+    /// Used for the first option button.
+    /// </summary>
+    string OptionOne { get; set; }
+    /// <summary>
+    /// The text for option two.
+    /// Used for the second option button.
+    /// </summary>
+    string OptionTwo { get; set; }
+    /// <summary>
+    /// The season in which the event occurs.
+    /// Should match with the enum in Game Manager.
+    /// </summary>
+    int Season { get; set; }
+    /// <summary>
+    /// The benefit/deficit of result one.
+    /// This will be added to whatever value is determined in the child classes.
+    /// </summary>
+    int ResultOne { get; set; }
+    /// <summary>
+    /// The benefit/deficit of result two.
+    /// This will be added to whatever value is determined in the child classes.
+    /// </summary>
+    int ResultTwo { get; set; }
+    /// <summary>
+    /// Has the event occurred?
+    /// This will prevent an event from repeating, if we use random selection.
+    /// </summary>
+    bool EventOccurred { get; set; }
+
+    /// <summary>
+    /// The action taken by the event.
+    /// Every event will have one, but each class will implement it differently.
+    /// </summary>
+    void EventAction();
+}
