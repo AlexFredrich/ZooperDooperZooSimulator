666fbbf303d2b8f01cc9b598f4bf8763b19e41d6
Joshua Whittom <Joshua.Whittom@iam.local>
2018-04-03 18:00:47 -0500

    More Scripts Added
    Game Manager and Money Event added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+    /// <summary>
+    /// The seasons, used for events and the like
+    /// </summary>
+    public enum SEASONS { SPRING, SUMMER, FALL, WINTER };
+
+    /// <summary>
+    /// List of animals in the game
+    /// </summary>
+    [SerializeField]
+    List<Animal> animals;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Assets/Scripts/MoneyEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyEvent : MonoBehaviour, IZooEvent
+{
+    public string DescriptionText { get; set; }
+    public string OptionOne { get; set; }
+    public string OptionTwo { get; set; }
+    public int Season { get; set; }
+    public int ResultOne { get; set; }
+    public int ResultTwo { get; set; }
+    public bool EventOccurred { get; set; }
+
+    public void EventAction()
+    {
+        throw new System.NotImplementedException();
+    }
+}
